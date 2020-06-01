cf9a67f2dbd39091d9e2abd7cf3d973316d9541b
João Soares <jpcirqueira81@gmail.com>
2020-06-01 18:02:25 -0300

    ajustado projeto para ts.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,13 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
+
+app.get('/', (request, response) => {
+    //alert('listagem')
+
+    response.json([
+        'inicio',
+    ]);
+});
+
+app.listen(3333); 
