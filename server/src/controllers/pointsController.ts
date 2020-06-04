9a60125b779e64a657b2b077314bb3e9b93a07ef
João Soares <jpcirqueira81@gmail.com>
2020-06-04 20:48:05 -0300

    ajustes no server.

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -51,7 +51,7 @@ class PointsController {
     
         const trx = await knex.transaction(); 
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name,
             email,
             whatsapp,
@@ -72,7 +72,7 @@ class PointsController {
         await trx('point_items').insert(pointItems);
         
         await trx.commit();
-        
+
         return response.json({ 
             id: point_id,
             ... point,
