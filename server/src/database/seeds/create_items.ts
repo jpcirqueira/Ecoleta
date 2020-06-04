81e2386c58d8f60a88336e8a7e056a8adf82cf2c
João Soares <jpcirqueira81@gmail.com>
2020-06-03 22:26:56 -0300

    adicionado listagem de items.

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -1,12 +1,12 @@
 import Knex from 'knex';
 
 export async function seed(knex: Knex) {
-    await knex('items').insert([
-        {title: 'Lampadadas', image: 'lampadas.svg' },
-        {title: 'Pilhas e Baterias', image: 'baterias.svg' },
-        {title: 'Papeis e Papelao', image: 'papeis-papelao.svg' },
-        {title: 'Residos Eletronicos', image: 'eletronicos.svg' },
-        {title: 'Residos Organicos', image: 'organicos.svg' },
-        {title: 'Oleo de cozinha', image: 'oleo.svg' },
-    ]);
+  await knex('items').insert([
+    { title: 'Lâmpadas', image: 'lampadas.svg' },
+    { title: 'Pilhas e Baterias', image: 'baterias.svg' },
+    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
+    { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
+    { title: 'Residuos Orgânicos', image: 'organicos.svg' },
+    { title: 'Óleo de Cozinha', image: 'oleo.svg' },
+  ]);
 }
